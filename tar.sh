92718cf122d58f6646183cfb2ef2915e334c4979
Po-Wei Chou <boton@master.ee.ntu.edu.tw>
2014-03-29 18:16:04 +0800

    Change tar.sh from ln -sf back to cp

--- tar.sh
+++ tar.sh
@@ -3,4 +3,4 @@ gzip -c rootfs | dd of=rootfs.gz
 chmod 755 rootfs.gz
 
 # Create symbolic link of linux root filesystem image
-ln -sf $(pwd)/rootfs.gz /tftpboot/linux/rootfs.gz
+cp rootfs.gz /tftpboot/linux/
